3b79ee4570ee6cb1baf18c76ad56becb1dd52ad5
jcao121 <jcao121@gmail.com>
2022-06-06 11:11:43 -0700

    JOptionPane message dialog

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {     
-   public static void main(String[] args) {        
-   System.out.println("Hello World");
+import javax.swing.JOptionPane;
+
+public class HelloWorld  {     
+   public static void main(String[] args) {   
+	   JOptionPane.showMessageDialog(null, "Hello World", "Hello World", JOptionPane.PLAIN_MESSAGE);
 }
 }
